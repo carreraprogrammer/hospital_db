5cf8e1982eb3e428d926d552867737e38cf714df
Daniel <carreraprogrammer@gmail.com>
2023-06-14 19:29:00 -0500

    Index for faster searching of invoice items by invoice id

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -84,3 +84,8 @@ FOREIGN KEY (invoice_id) REFERENCES invoices(id)
 
 CREATE INDEX treatment_id_invoice_items_index
 ON invoice_items (treatment_id);
+
+-- Index for faster searching of invoice items by invoice id
+
+CREATE INDEX invoice_id_invoice_items_index
+ON invoice_items (invoice_id);
