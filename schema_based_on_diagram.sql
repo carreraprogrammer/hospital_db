aaa68b6fa0c546da756f008865fa51b7c6ee3f5d
Daniel <carreraprogrammer@gmail.com>
2023-06-14 19:19:29 -0500

    Add Table to store medical history information

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,13 @@ id int PRIMARY KEY IDENTITY (1,1),
 name varchar,
 date_of_birth date
 );
+
+-- Table to store medical history information
+
+CREATE TABLE medical_histories(
+id int PRIMARY KEY IDENTITY (1,1),
+admitted_at timestamp,
+patient_id int,
+FOREIGN KEY (patient_id) REFERENCES patients(id),
+status varchar
+);
